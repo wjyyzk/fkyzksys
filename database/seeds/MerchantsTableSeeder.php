addf4f06b744359c75bfbe29cfbf6d66cd42af5d
dev <roy.wijaya-dit@jp.yazaki.com>
2017-01-23 15:28:41 +0900

    業者Seederを更新した

--- database/seeds/MerchantsTableSeeder.php
+++ database/seeds/MerchantsTableSeeder.php
@@ -12,119 +12,148 @@ class MerchantsTableSeeder extends Seeder
     public function run()
     {
         DB::table('merchant')->insert([
-            'name'         =>  '弘洋',
+            'name'			=>  '弘洋',
+            'furigana'		=>	'コウヨウ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '横浜テクニカ',
+            'name'			=>  '横浜テクニカ',
+            'furigana'		=>	'ヨコハマテクニカ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '矢崎イヒエ',
+            'name'			=>  '矢崎化工',
+            'furigana'		=>	'ヤザキカコウ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'モテック',
+            'name'			=>  'モテック',
+            'furigana'		=>	'モテック'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '松浦製作所',
+            'name'			=>  '松浦製作所',
+            'furigana'		=>	'マツウラセイサクショ' 
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '増田工業',
+            'name'			=>  '増田工業',
+            'furigana'		=>	'マスダコウギョウ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'フケン販売',
+            'name'			=>  'フケン販売',
+            'furigana'		=>	'フケンハンバイ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '深谷製作所',
+            'name'			=>  '深谷製作所',
+            'furigana'		=>	'フカヤセイサクショ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'ファムテック',
+            'name'			=>  'ファムテック',
+            'furigana'		=>	'ファムテック'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '花澤製作所',
+            'name'			=>  '花澤製作所',
+            'furigana'		=>	'ハナザワセイサクショ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'ナカモク',
+            'name'			=>  'ナカモク',
+            'furigana'		=>	'ナカモク'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'エヌテック',
+            'name'			=>  'エヌテック',
+            'furigana'		=>	'エヌテック'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'MPA',
+            'name'			=>  'MPA',
+            'furigana'		=>	'MPA'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '大東機械',
+            'name'			=>  '大東機械',
+            'furigana'		=>	'ダイトウキカイ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'HIB専用機',
+            'name'			=>  'HIB専用機',
+            'furigana'		=>	'HIBセンヨウキ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'ISJ',
+            'name'			=>  'ISJ',
+            'furigana'		=>	'ISJ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'MKH',
+            'name'			=>  'MKH',
+            'furigana'		=>	'MKH'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '野中技研',
+            'name'			=>  '野中技研',
+            'furigana'		=>	'ノナカギケン'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'NIM',
+            'name'			=>  'NIM',
+            'furigana'		=>	'NIM'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'OMP',
+            'name'			=>  'OMP',
+            'furigana'		=>	'OMP'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'SSN',
+            'name'			=>  'SSN',
+            'furigana'		=>	'SSN'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'TNS',
+            'name'			=>  'TNS',
+            'furigana'		=>	'TNS'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'TCS',
+            'name'			=>  'TCS',
+            'furigana'		=>	'TCS'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '植田部品',
+            'name'			=>  '植田部品',
+            'furigana'		=>	'ウエダブヒン'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '湖西機工',
+            'name'			=>  '湖西機工',
+            'furigana'		=>	'コサイキコウ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '佐野',
+            'name'			=>  '佐野',
+            'furigana'		=>	'サノ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  'WSZ',
+            'name'			=>  'WSZ',
+            'furigana'		=>	'WSZ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '先生鉄工所',
+            'name'			=>  '先生鉄工所',
+            'furigana'		=>	'センセイテッコウジョ'
         ]);
 
         DB::table('merchant')->insert([
-            'name'         =>  '長野工業',
+            'name'			=>  '長野工業',
+            'furigana'		=>	'ナガノコウギョウ'
         ]);
     }
 }
