89bf0adb3a0ec05dd757123cb46ed16eaab26f40
dev <roy.wijaya-dit@jp.yazaki.com>
2017-02-07 11:13:01 +0900

    印刷表示の修正

--- resources/views/admin/print/print.blade.php
+++ resources/views/admin/print/print.blade.php
@@ -4,49 +4,55 @@
 		<style type="text/css">
 			html
 			{
-				margin: 0px;
-				padding: 0px;
-			}
-			body
-			{
 				width: 340px;
 				height: 151px;
-				margin-top: 5px;
-				margin-left: 5px;
+				margin: 10px;		
+				padding: 0px;
 			}
 			label
 			{
 				font-family: "Meiryo UI";
-				font-size: 16px;
+				font-size: 20px;
+				font-weight: bold;
 			}
 			.page-break
 			{
 				page-break-after: always;
 			}
-			.mymargin
+			.column {
+				float: left;
+			}
+			.clearfix::after {
+				content: "";
+				clear: both;
+				display: table;
+			}
+			.barcode {
+				width: 40%;
+				position: absolute;
+				bottom: 0;
+			}
+			.content {
+				width: 60%;
+				overflow-wrap: break-word;
+			}
+			.lbluniqueid
 			{
-				margin: 5px;
+				position: absolute;
+				bottom: 12;
 			}
 		</style>
 	</head>
 	<body>
-		<table width="100%">
-			<tr>
-				<td colspan="2" class="mymargin"><label>{{ $model->chikouguhinban }}</label></td>		
-			</tr>
-			<tr>
-				<td valign="top" class="mymargin"><label style="font-size: 20px;">{{ $model->hinban }}</label></td>
-				<td rowspan="2" valign="bottom" style="text-align: right;">
-					<img class="qrcode" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(120)->generate($model->id.'*'.$model->hinban.'*'.$model->chikouguhinban)) !!} ">
-				</td>				
-			</tr>
-			<tr>
-				<td class="mymargin"><label>{{ $model->id }}</label></td>
-			</tr>
-		</table>
-		<!--	複数ページのため
-			<div class="page-break"></div>
-			<h1>2nd page</h1>
-		-->
+		<div class="clearfix">
+			<div class="column content">
+				<div><label>{{ $model->chikouguhinban }}</label></div>
+				<div><label style="font-size: 24px;">{{ $model->hinban }}</label></div>
+				<div class="lbluniqueid"><label>{{ $model->id }}</label></div>
+			</div>
+			<div class="column barcode">
+				<img class="qrcode" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(150)->generate($model->id.'*'.$model->hinban.'*'.$model->chikouguhinban)) !!} ">
+			</div>
+		</div>
 	</body>
 </html>
