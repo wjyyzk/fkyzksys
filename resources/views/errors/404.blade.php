de438bde43221e821407a85e4097a13d47f3f46b
dev <roy.wijaya-dit@jp.yazaki.com>
2017-03-08 08:23:52 +0900

    エラーページの更新

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<title>深谷在庫システム　-　メンテナンス</title>
+<style>
+    body {
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article>
+    <center>
+        <h1>404エラー</h1>
+        <div>
+            <p>
+                ページが見つかりません<br/>
+                <a href="/">ホームへ移動</a>
+            </p>
+        </div>
+    </center>
+</article>
