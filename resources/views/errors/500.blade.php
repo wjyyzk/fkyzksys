e7234ad95e02f1d5120f47268076dfe184a1de41
Roy <roy.wijaya-dit@jp.yazaki.com>
2017-12-20 08:46:57 +0900

    エラー500ページの追加

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<title>深谷在庫システム　-　エラー</title>
+<style>
+    body {
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article>
+    <center>
+        <h1>500エラー</h1>
+        <div>
+            <p>
+                サーバーエラーが発生しました。<br/>
+                <a href="/">ホームへ移動</a>
+            </p>
+        </div>
+    </center>
+</article>
